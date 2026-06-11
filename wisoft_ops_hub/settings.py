25f064b3792b396022141f4974bac5a8795661c9
MAC <mac@MACs-MacBook-Pro.local>
2026-06-11 17:58:25 +0400

    add git igonre, static and templates

--- wisoft_ops_hub/settings.py
+++ wisoft_ops_hub/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'wisoft_ops_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,5 +104,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
