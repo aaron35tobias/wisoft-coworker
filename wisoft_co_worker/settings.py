1ce7d5a35ec7ce025cba466570b2f9479e93184d
prxcode <artistic.py@gmail.com>
2026-07-02 11:01:13 +0400

    feat: dynamic ai api usage tracking

--- wisoft_co_worker/settings.py
+++ wisoft_co_worker/settings.py
@@ -20,6 +20,7 @@ SITE_TESTING_URL = os.environ['SITE_TESTING_URL']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.humanize',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
